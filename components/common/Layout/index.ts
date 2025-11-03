a492eadec0ea3083ee0030cb5ce196c8aabeac63
albert <albertamoah2000@gmail.com>
2025-11-03 23:18:08 +0000

    removed all unnecessary

--- components/common/Layout/index.ts
+++ components/common/Layout/index.ts
@@ -1,6 +1,1 @@
 export { ResponsiveContainer } from './ResponsiveContainer';
-export { SplitViewLayout } from './SplitViewLayout';
-export { ResponsiveGrid } from './ResponsiveGrid';
-export { ResponsiveModal } from './ResponsiveModal';
-export { ResponsiveForm } from './ResponsiveForm';
-export { ResponsiveTable } from './ResponsiveTable';
